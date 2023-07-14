9f14a04f1f6bfc678c1cb62e00c63283d14410cc
Sébastien <sebastien.arbaud@hotmail.com>
2023-07-14 11:04:06 +0200

    ajout fichier Phill dans home

--- components/Home.js
+++ components/Home.js
@@ -2,29 +2,51 @@ import styles from '../styles/Home.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../reducers/user';
 
-
-
+const dispatch = useDispatch();
+const user = useSelector((state) => state.user.value);
 
 function Home() {
 
-  const dispatch = useDispatch();
-const user = useSelector((state) => state.user.value);
-
-	const handleLogout = () => {
-		dispatch(logout());
-	};
+const handleLogout = () => {
+    dispatch(logout());
+};
 
   
-
   return (
     <div>
       <main className={styles.main}>
-        <h6 className={styles.title}>
-          <p>Welcome to home</p>
-        </h6>
-      </main>
+
+    <div className={boxLeft}>
+      <div className={styles.logoTop}><img src='logo.png'></img></div>
+      <div className={styles.user}>
+        <div>
+          <img></img>
+          <h2>{user.firstName}</h2>
+          <h5>@{user.username}</h5>
+        </div>
+        <button className={styles.logout}></button>
+      </div>
     </div>
+
+    <div className={boxCenter}>
+        <div>
+          <h1 className={styles.title}>Home</h1>      
+          <button className={styles.tweet}></button>     
+        </div>
+        <div className={styles.tweets}></div>
+    </div>
+    
+    <div className={boxRight}>
+      <h1 className={styles.trends}></h1>
+      <div className={styles.hashtags}>
+
+      </div>
+    </div>
+
+  </main>
+</div>
   );
 }
 
 export default Home;
+
